6b9ae0dfbee78cdcbf584b4a80af2f01a29fca4f
Manuel <manuel.simon@gmail.com>
2017-04-22 15:54:15 +0200

    ajout test mocha

---
+++ index.js
@@ -0,0 +1,3 @@
+exports.log = function() {
+  console.log("Hello world !");
+}
---
+++ test/test.js
@@ -0,0 +1,5 @@
+var assert = require('assert');
+describe('Log', function() {
+  describe('simple log', function() {
+  });
+});
